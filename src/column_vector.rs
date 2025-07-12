4b2713be75bc0d62f33523ebc08ceeb6ce6f2b01
GStechschulte <stechschulteg@gmail.com>
2025-07-12 09:04:49 +0200

    refactor

--- src/column_vector.rs
+++
@@ -1,8 +0,0 @@
-// TODO: ...
-
-pub trait ColumnVector {
-    /// Interface that provides convienent accessor methods to avoid the need
-    /// to case to a specific `FieldVector` implementation for each Arrow array type.
-    fn get_type(&self);
-    fn get_value(&self);
-}
